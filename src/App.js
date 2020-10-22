57646a192c39da3a1120d97c925e285077c3d1aa
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-10-22 19:14:32 +0530

    working fix

--- src/App.js
+++ src/App.js
@@ -1,50 +1,9 @@
-import React,{useState} from 'react';
+import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import axios from "axios"
+import Home from "./Components/Home"
 
 function App() {
-
-  const [file,setFile] = useState()
-
-  const fileChangedHandler = (event) => {
-    let upload_file = event.target.files[0];
-    console.log(upload_file)
-    
-    
-    
-  };
-  console.log(file)
-
-  const uploadFile = (event) =>{
-      event.preventDefault()
-      const data = new FormData()
-      data.append("file",file)
-      axios.post("http://localhost:5000/upload",data)
-      .then((res)=>res.json())
-      .then((data)=>console.log(data))
-      .catch((error)=>console.log(error))
-
-  }
-
-  const Home = ()=>{
-    return (
-      <form method="POST" action="http://localhost:5000/upload" encType="multipart/form-data">
-        <div> 
-            <label>Select your file:</label> 
-            <input onChange={fileChangedHandler} type="file" name="file"/>
-        </div>
-        <div>
-          <button className="btn btn-primary mt-2" type="submit" name="file_upload" value="Upload">
-            Upload
-          </button>
-        </div>
-      </form>
-    )
-  }
-
-  
-
   
   return (
   <Router>
---
+++ src/Components/Home.js
@@ -0,0 +1,58 @@
+import React,{useState} from "react"
+import axios from "axios"
+
+const Home = ()=>{
+
+  const [file,setFile] = useState()
+  const [islink,setIslink] = useState(false)
+  const [link,setLink] = useState("")
+  
+
+  const upload = event => {
+    event.preventDefault()
+    const data = new FormData();
+    data.append("file", file);
+
+    axios.post("http://localhost:5000/upload", data)
+      .then(res => {
+          console.log(res.data)
+          alert(res.data.message)
+          setIslink(true)
+          setLink(res.data.shorturl)
+          setFile()
+        }
+      )
+      .catch(err => console.log(err));
+  };
+
+  const again = ()=>{
+    window.location.reload()
+  }
+
+    return (
+        <>
+      <form onSubmit={upload}>
+        <div> 
+            <label>Select your file:</label> 
+            <input onChange={event=>{
+                const file =  event.target.files[0];
+                setFile(file)
+            }} type="file" name="file"/>
+        </div>
+        <div>
+          <button className="btn btn-primary mt-2" type="submit" name="file_upload" value="Upload">
+            Upload
+          </button>
+        </div>
+      </form>
+      {islink ? <div>
+                <p>Your One time Link :</p>
+                    <h4>{link}</h4>
+                <button onClick={again} className="btn btn-success">Upload Another</button>
+                </div> 
+          : <div></div>}
+      </>
+    )
+  }
+
+export default Home;
